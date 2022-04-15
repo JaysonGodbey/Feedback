b2a020b9f4e4d62e89a2eaf0dcd0ce581a15a61a
Jayson Godbey <43347663+JaysonGodbey@users.noreply.github.com>
2022-04-15 16:05:15 -0400

    Added Feedback input, Rating Input, Calculations, UX Improvements

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,17 +1,47 @@
 import { useState } from "react"
 import Card from "./shared/Card"
+import Button from "./shared/Button"
+import RatingSelect from "./RatingSelect"
 
-function FeedbackForm(){
+function FeedbackForm({handleAdd}){
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+    const [rating, setRating] = useState(10)
 
     const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 8){
+            setMessage('Text must be at least 10 characters')
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+        
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating,
+            }
+
+            handleAdd(newFeedback)
+
+            setText('')
+        }
+    }
+
     return <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service with us?</h2>
-            {}
+            <RatingSelect select={(rating) => setRating(rating)}/>
             <div className="input-group">
                 <input 
                     onChange={handleTextChange} 
@@ -19,8 +49,10 @@ function FeedbackForm(){
                     placeholder="Write a review" 
                     value={text}
                 />
-                <button type='submit'>Send</button>
+                <Button type='submit' isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {message && <div className="message">{message}</div>}
         </form>
     </Card>
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import 'C://Users/river/Feedback/Feedback/src/index.css'
 import App from './App'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>, 
-    document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
